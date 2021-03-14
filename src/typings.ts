a44f47eed34eab3165335640f990b5f4dd198bd6
Peter Logo <peterlogo77@gmail.com>
2021-03-13 21:54:58 -0500

    New cache methods added

--- src/typings.ts
+++ src/typings.ts
@@ -30,5 +30,8 @@ export interface IRedisCacheService {
   checkTime: (key: string) => Promise<number | undefined>;
   set: (key: string, value: string, exp?: number) => Promise<string | undefined>;
   get: (key: string) => Promise<string | null>;
+  del: (key: string) => Promise<string | number>;
   multiSet: (items: ICacheProp[]) => Promise<string | boolean>;
+  multiGet: (keys: string[]) => Promise<string | string[]>;
+  multiDel: (keys: string[]) => Promise<string | number>;
 }
